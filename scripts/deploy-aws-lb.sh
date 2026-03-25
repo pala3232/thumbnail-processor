cbbe6c059702bfb3707fb40daab24d359d0df47e
yo <palamp3105@gmail.com>
2026-03-25 21:10:35 +1300

    removed helm manual conflictive steps

--- scripts/deploy-aws-lb.sh
+++ scripts/deploy-aws-lb.sh
@@ -3,15 +3,9 @@ set -e
 
 ALB_ROLE_ARN=$(cd ../terraform && terraform output -raw alb_controller_role_arn)
 CLUSTER_NAME=$(cd ../terraform && terraform output -raw cluster_name)
-echo "downloading/applying CRDs for AWS Load Balancer Controller..."
-
-wget https://raw.githubusercontent.com/aws/eks-charts/master/stable/aws-load-balancer-controller/crds/crds.yaml
-kubectl apply --server-side --force-conflicts -f crds.yaml
-echo "adding eks..."
+echo "adding eks helm repo..."
 
 helm repo add eks https://aws.github.io/eks-charts
-echo "done adding EKS! updating EKS now..."
-
 helm repo update eks
 echo "done updating EKS! installing AWS Load Balancer Controller now..."
 
@@ -21,6 +15,7 @@ helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-contro
   --set serviceAccount.create=true \
   --set serviceAccount.name=aws-load-balancer-controller \
   --set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=$ALB_ROLE_ARN" \
+  --set crds.keep=true \
   --version 1.14.0
 
 echo "done installing AWS Load Balancer Controller!"
