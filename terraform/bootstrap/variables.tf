99829911219be5133b896434001606ff259d0925
yo <palamp3105@gmail.com>
2026-03-25 21:38:18 +1300

    added terraform bootstrap module

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,11 +1,1 @@
-variable "tfstate_bucket" {
-  description = "Name of the S3 bucket for Terraform remote state"
-  type        = string
-  default     = "tfstate-pala3105"
-}
-
-variable "github_repo" {
-  description = "GitHub repo in owner/repo format"
-  type        = string
-  default     = "pala3232/thumbnail-processor"
-}
+# Moved to /bootstrap-terraform/
