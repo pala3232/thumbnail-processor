e6c39ebf9dac6d2ff1b31561e52792e9b3544ae5
yo <palamp3105@gmail.com>
2026-03-26 18:20:35 +1300

    fixed script and deploy all workflow

--- scripts/deploy-keda.sh
+++ scripts/deploy-keda.sh
@@ -2,7 +2,7 @@
 set -e
 echo "adding KEDA repo, downloading KEDA..."
 
-helm repo add kedacore https://kedacore.github.io/charts
+helm repo add kedacore https://kedacore.github.io/charts 2>/dev/null || true
 echo "repo added. updating it now..."
 
 helm repo update kedacore
